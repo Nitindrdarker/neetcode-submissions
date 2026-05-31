42e9994f32bde93e59481c5f45a17902bca380bd
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-06-01 00:01:30 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.rob2(nums[:-1]), self.rob2(nums[1:]))
+
+    def rob2(self, nums: List[int]) -> int:
+        memo = [0 for i in range(len(nums) + 2)]
+        memo[len(nums)] = 0
+        def util(index):
+            if index >= len(nums):
+                return 0
+            if memo[index] != 0:
+                return memo[index]
+            take = nums[index] + util(index + 2)
+            skip = util(index+1)
+            memo[index] = max(take, skip)
+            return memo[index]
+        print(util(0))
+        return util(0)
+    
+        
