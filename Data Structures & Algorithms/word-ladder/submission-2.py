16fd35e13cfddd92b585e4e3aac6761f5dbbea9c
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-06-21 00:06:58 +0530

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def __init__(self):
+        self.wordMap = defaultdict(set)
+
+    def createWordMap(self, word):
+        for i in range(len(word)):
+            currHash = word[:i] + "*" + word[i+1:]
+            self.wordMap[currHash].add(word)
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = [beginWord] + wordList
+        for word in wordList:
+            self.createWordMap(word)
+        q = collections.deque()
+        q.append((beginWord, 1))
+        visited = set()
+        while q:
+            word, t = q.popleft()
+            if word == endWord:
+                return t
+            
+            for i in range(len(word)):
+                currHash = word[:i] + "*" + word[i+1:]
+                for neigh in self.wordMap[currHash]:
+                    if neigh not in visited:
+                        visited.add(neigh)
+                        q.append((neigh, t + 1))
+        return 0
+            
+
+                
+
+                
+
+
+
+
+
+
+
+        
+        
