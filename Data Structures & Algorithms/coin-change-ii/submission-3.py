aac2cc4e01bfeaddbc8b73dbed95b0c490187f88
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-06-27 17:15:51 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.py
@@ -0,0 +1,44 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # memo = [[0 for i in range(amount + 1)] for j in range(len(coins) + 1)]
+
+        # for index in range(len(coins) - 1, -1, -1):
+        #     for curr in range(amount, -1, -1):
+        #         if curr == amount:
+        #             memo[index][curr] = 1
+        #             continue
+        #         if curr > amount:
+        #             continue
+        #         v = 0
+                
+        #         for i in range(index, len(coins)):
+                
+        #             temp = memo[i][curr + coins[i]] if curr + coins[i] <= amount else 0
+                    
+        #             v += temp
+        #         memo[index][curr] = v
+        # return memo[0][0]
+                
+
+
+
+
+
+
+        memo = {}
+        def util(index, curr):
+            if curr == amount:
+                return 1
+            if curr > amount:
+                return 0
+            if index >= len(coins):
+                return 0
+            if (index, curr) in memo:
+                return memo[(index, curr)]
+            
+            take = util(index, curr + coins[index])
+            skip = util(index+1, curr)
+            memo[(index, curr)] = take + skip
+            return memo[(index, curr)]
+        return util(0, 0)
+            
