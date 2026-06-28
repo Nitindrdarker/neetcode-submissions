006792df69583e1692a2bd284b03e8e43b3e6795
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-06-28 20:47:41 +0530

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        def util(index, curr):
+            
+            
+            if index >= len(nums):
+                if curr == target * 2: 
+                    return 1
+                return 0
+            if (index, curr) in memo:
+                return memo[(index, curr)]
+            add = util(index+1, curr + nums[index])
+            sub = util(index+1, curr - nums[index])
+            memo[(index, curr)] = add + sub
+            return memo[(index, curr)]
+        return util(0, target)
