8b1ce988288399592d1a50434692f8d3ad263136
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-07-07 08:16:35 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        sign = 1
+        isInverse = False
+        if n < 0:
+            isInverse = True
+            n = -1 * n
+        if x < 0 and n % 2:
+            sign = -1
+            
+
+        def calc(p):
+            if p <= 0:
+                return 1
+            if p == 1:
+                return x
+            if p % 2:
+                return x * calc(p // 2) * calc(p // 2)
+            return calc(p // 2) * calc(p // 2)
+        v = calc(n)
+        if isInverse:
+            v = 1 / v
+        return sign * v
