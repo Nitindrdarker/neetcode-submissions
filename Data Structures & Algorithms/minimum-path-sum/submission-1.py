80c5edbfa8ce12dc11cb3cb8ff01d1dfdd20aa32
Nitin <63086591+Nitindrdarker@users.noreply.github.com>
2026-08-10 08:01:17 +0530

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        heap = [(grid[0][0], 0, 0)]
+        row = len(grid)
+        col = len(grid[0])
+        visited = set()
+        while heap:
+            s, i, j = heapq.heappop(heap)
+            if i == row - 1 and j == col - 1:
+                return s
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+            for r, c in ([1, 0], [0, 1]):
+                nr, nc = r + i, c + j
+                if nr < row and nc < col:
+                    heapq.heappush(heap, (s + grid[nr][nc], nr, nc))
+        return -1
+            
+
+        
